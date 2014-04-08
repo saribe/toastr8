5451b826b166ddbee985582a4598c51195eb8dd5
Samuel Pinto <samuel@maispc.com>
2014-04-08 14:38:59 +0100

    New features
    Now toastr8 allows to include images url to toast, and img css class.
    Toast size is smaller and responsive design

--- app/main.js
+++ app/main.js
@@ -1,41 +1,34 @@
 ﻿requirejs.config({
     paths: {
         'text': '../lib/require/text',
-        'toastr': '../lib/toastr/js/toastr',
         'toastr8': '../lib/toastr8/js/toastr8',
         'jquery': '../lib/jquery/jquery-1.9.1'
     }
 });
 
-define(['toastr', 'toastr8'], function (toastr, toastr8) {
-
-    
+define(['toastr8'], function (toastr8) {
 
     var init = function () {
-        toastr8.info("Go get him a nice meal...", "It is your dog birthay.", { newestOnTop: true, timeOut: (Math.random() * 21200) + 1 }).css("background-color", getRandomColor());
-        toastr8.warning("This is a beautiful message body....", "It is your dog birthay!", { newestOnTop: true, timeOut: (Math.random() * 831020) + 1 }).css("background-color", getRandomColor());
-        toastr8.error("This is a beautiful message body....", "It is your dog birthay!", { newestOnTop: true, timeOut: (Math.random() * 11400) + 1 }, "toast8-default").css("background-color", getRandomColor());
-        toastr8.success("This is a beautiful message body.This is a beautiful message body.This is a beautiful message body....", "It is your dog birthay!", { newestOnTop: true, timeOut: (Math.random() * 10300) + 1 }).css("background-color", getRandomColor());
+        toastr8.info("Go get him a nice meal...", "It is your dog birthay.", { timeOut: (Math.random() * 21200) + 1 });
+        toastr8.warning("This is a beautiful message body....", "It is your dog birthay!", { timeOut: (Math.random() * 831020) + 1 });
+        toastr8.error("This is a beautiful message body....", "It is your dog birthay!");
+        toastr8.success("This is a beautiful message body..", "It is your dog birthay!");
 
 
-        toastr8.github("This is a beautiful message body.", "Alerta github", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 });
-        toastr8.googlePlus("This is a beautiful message body.", "Alerta Google+", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 });
-        toastr8.linux("This is a beautiful message body.", "Alerta Tuxxx", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 });
-        toastr8.facebook("This is a beautiful message body....", "Notificação Facebook", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 }, "toast8-avatar");
-        toastr8.twitter("This is a beautiful message body....", "Alerta Twitter", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 });
-        toastr8.windows("This is a beautiful message body....", "Alerta Windows 8", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 });
-        toastr8.linkedin("This is a beautiful message body....", "Alerta LinkdeIn", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 }, "toast8-avatar");
-        toastr8.android("This is a beautiful message body....", "Alerta Android", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 });
-        toastr8.skype("This is a beautiful message body....", "Alerta Skype", { newestOnTop: true, timeOut: (Math.random() * 811000) + 1 });
+        toastr8.github("This is a beautiful message body.", "Alerta github", { timeOut: (Math.random() * 811000) + 1 });
+        toastr8.googlePlus("This is a beautiful message body.", "Alerta Google+", { timeOut: (Math.random() * 811000) + 1 });
+        toastr8.linux("This is a beautiful message body.", "Alerta Tuxxx");
+        toastr8.facebook("This is a beautiful message body....", "Notificação Facebook", { timeOut: (Math.random() * 811000) + 1 }, "toast8-avatar");
+        toastr8.twitter("This is a beautiful message body....", "Alerta Twitter", { timeOut: (Math.random() * 811000) + 1 });
+        toastr8.windows("This is a beautiful message body....", "Alerta Windows 8", { timeOut: (Math.random() * 811000) + 1 });
+        toastr8.linkedin("This is a beautiful message body....", "Alerta LinkdeIn", { timeOut: (Math.random() * 811000) + 1 }, "toast8-avatar");
+        toastr8.android("This is a beautiful message body....", "Alerta Android", { timeOut: (Math.random() * 811000) + 1 });
+        toastr8.skype("This is a beautiful message body....", "Alerta Skype", { timeOut: (Math.random() * 811000) + 1 });
         return this;
     };
 
     var init2 = function () {
-        toastr.options.positionClass = 'toast8-bottom-left';
-        toastr.info("This is a beautiful message body....", "It is your dog birthay!");
-        toastr.warning("This is a beautiful message body....", "It is your dog birthay!");
-        toastr.error("This is a beautiful message body....", "It is your dog birthay!");
-        toastr.success("This is a beautiful message body....", "It is your dog birthay!");
+      
     };
 
     function getRandomColor() {
@@ -50,11 +43,11 @@ define(['toastr', 'toastr8'], function (toastr, toastr8) {
     $("#throwToastr8").click(init);
     $("#throwToastr").click(init2);
     $("#throwGithub").click(function () {
-        toastr8.github("<a href='https://github.com/saribe/toastr8'><i class='fa fa-link'></i> GitHub...</a>", "Toastr8 Page", { newestOnTop: true, timeOut: (Math.random() * 911400) + 1 }, "toastr-logo");
+        toastr8.github("<a href='https://github.com/saribe/toastr8'><i class='fa fa-link'></i> GitHub...</a>", "Toastr8 Page", { timeOut: (Math.random() * 911400) + 1 }, "toastr-logo");
     });
     
-    $("#throwPPage").click(function() {
-        toastr8.windows("<a href='http://samuel.maispc.com'><i class='fa fa-link'></i> About me...</a>", "Personal Page", { newestOnTop: true, timeOut: (Math.random() * 911400) + 1 }, "toast8-avatar", "fa fa-user");
+    $("#throwPage").click(function() {
+        toastr8.error("<a href='http://samuel.maispc.com'><i class='fa fa-link'></i> About me...</a>", "Personal Page", { }, "toast8-avatar", "fa fa-user").css("background-color",getRandomColor());
     });
    // $(".fa-cog:nth-child(3)").click(init3);
 
